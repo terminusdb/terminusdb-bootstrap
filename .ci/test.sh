4258d1dd9d2090d9ae55c42159eb57bb094008e0
Dmytri Kleiner <dk@trick.ca>
2020-05-07 15:01:50 +0200

    move tests to .ci

---
+++ .ci/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+shellcheck terminus-container
+../terminus-container help
+../terminus-container run
+nohup ./terminus-container console
+nohup ./terminus-container attach
+../terminus-container stop
+yes | ../terminus-container rm
+../terminus-container test
